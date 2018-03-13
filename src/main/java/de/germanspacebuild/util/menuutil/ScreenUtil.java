1064b0a4f9f1b52a641d18f6550045832bf0c9b4
oneill011990 <felix@kraas-family.de>
2018-03-14 00:09:12 +0100

    Documentation for ScreenUtil class

--- src/main/java/de/germanspacebuild/util/menuutil/ScreenUtil.java
+++ src/main/java/de/germanspacebuild/util/menuutil/ScreenUtil.java
@@ -11,18 +11,24 @@ import java.util.Scanner;
 /**
  * Created by Felix on 13.03.2018
  * for MenuUtil
- *
+ * <p>
+ * Contains utility for creating and working with Screens.
  * @author Felix
  */
 public class ScreenUtil {
 
     private static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Reads text from TXT file for use in menu.
+     * @param fileName Name of the file to be loaded. Should but must not end with .txt.
+     * @return Text read from the file.
+     */
     public static String readTextFile(String fileName) {
         String text = "";
         String line;
 
-        if (!fileName.contains(".txt")) {
+        if (!fileName.endsWith(".txt")) {
             StringBuilder sb = new StringBuilder(fileName);
             sb.append(".txt");
             fileName = sb.toString();
@@ -50,6 +56,10 @@ public class ScreenUtil {
         return text;
     }
 
+    /**
+     * Scans screen for tags to be replaced with text.
+     * @param screen Screen to be scanned.
+     */
     public static void scanReplacementTags(Screen screen) {
         final String text = screen.getText();
         List<String> words = Arrays.asList(text.split("(\\r?\\n)"));
@@ -66,6 +76,10 @@ public class ScreenUtil {
         }
     }
 
+    /**
+     * Reads input made by user.
+     * @return Input the user made.
+     */
     public static String readInput() {
         String s = scanner.nextLine();
         if (s == null || s.isEmpty()) {
@@ -75,10 +89,16 @@ public class ScreenUtil {
         return s;
     }
 
+    /**
+     * Resets Scanner to System.in
+     */
     private static void resetInput() {
         scanner = new Scanner(System.in);
     }
 
+    /**
+     * Clears the Screen completely.
+     */
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
     }
