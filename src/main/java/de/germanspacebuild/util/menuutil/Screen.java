b4d24938e1fb41e04c94f9e413bf56f2b1f938e8
oneill011990 <felix@kraas-family.de>
2018-03-14 03:58:52 +0100

    Documentation for Screen class

--- src/main/java/de/germanspacebuild/util/menuutil/Screen.java
+++ src/main/java/de/germanspacebuild/util/menuutil/Screen.java
@@ -16,10 +16,17 @@ public class Screen {
     private Map<String, String> replaceTags = new HashMap<>();
     private boolean awaitsInput = false;
 
+    /**
+     * Creates new screen.
+     * @param text Text of the screen.
+     */
     public Screen(String text) {
         this.text = text;
     }
 
+    /**
+     * Prints text to screen and replaces all tags with it's designated text.
+     */
     public void print() {
         String screen = text;
         for (String key : replaceTags.keySet()) {
@@ -31,31 +38,59 @@ public class Screen {
         }
     }
 
+    /**
+     * Closes screen and clears input and tags.
+     */
     public void close() {
         replaceTags.clear();
         input = "";
     }
 
+    /**
+     * Gets text of screen.
+     * @return Text of screen.
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Edit a tags replacement text.
+     * @param tag Tag to edit.
+     * @param replacement Replacement for the tag.
+     */
     public void editRaplaceTag(String tag, String replacement) {
         replaceTags.put(tag, replacement);
     }
 
+    /**
+     * Adds a new tag to the Screen.
+     * @param tag Tag to be added.
+     */
     public void addReplaceTag(String tag) {
         replaceTags.put(tag, "@WARNING_NO_REPLACEMENT@ " + tag);
     }
 
+    /**
+     * Gets all tags and there replacement.
+     * @return All tags and there replacement.
+     */
     public Map<String, String> getReplaceTags() {
         return replaceTags;
     }
 
+    /**
+     * Get current input for the screen.
+     * @return Current input.
+     */
     public String getInput() {
         return input;
     }
 
+    /**
+     * Set the screen to await input from user.
+     * @param awaitsInput Whether or not the screen awaits input from the user.
+     */
     public void setAwaitsInput(boolean awaitsInput) {
         this.awaitsInput = awaitsInput;
     }
