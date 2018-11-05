4b12e6e05a43170abc9eec3a709d39280309b06a
Felix Kraas <felix@kraas-family.de>
2018-11-05 01:59:28 +0100

    ActionHandler Annotation

---
+++ src/main/java/de/germanspacebuild/util/menuutil/ActionHandler.java
@@ -0,0 +1,12 @@
+package de.germanspacebuild.util.menuutil;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActionHandler {
+    String id();
+}
